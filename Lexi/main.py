cfa8d69e5ad15e26bd4a2f865dedb6d9159f04ee
arezou <pakzadarezou@gmail.com>
2019-04-07 15:11:52 +0430

    first tries

--- Lexi/main.py
+++ Lexi/main.py
@@ -1,1 +1,25 @@
-print("Hello Lexi!!")
+input_file = open("input.txt", 'r')
+code = input_file.read()
+ind = 0
+current_char = ''
+
+digit = [str(i) for i in range(10)]
+alphabet = [chr(i) for i in range()]
+
+
+def get_next_char():
+    global ind, code, current_char
+    current_char = code[ind]
+    ind += 1
+    return current_char
+
+
+def get_next_token():
+    while get_next_char() is not None:
+        if current_char
+
+
+
+
+
+
