f664c598dce0bd958c112ae1a485301f74ecfac0
arezou <pakzadarezou@gmail.com>
2019-04-11 11:30:04 +0430

    printing the results and debug2

--- Lexi/main.py
+++ Lexi/main.py
@@ -3,19 +3,25 @@ import sys
 
 input_file = open("input.txt", 'r')
 code = input_file.read()
+
+output_file = open("scanner.txt", 'a')
+
+error_file = open("lexical_errors.txt", 'a')
+
 start_ind = 0
 end_ind = 0
 ind = -1
 current_char = ''
 next_char = ''
-line = 0
+line_num = 1
+line_changed = True
 
 TOKEN_INITIAL = 0
 TOKEN_NUM = 1
 TOKEN_KEYWORD = 2
 TOKEN_ID = 3
 TOKEN_SYMBOL = 4
-TOKEN_COMMNET = 5
+TOKEN_COMMENT = 5
 TOKEN_WHITESPACE = 6
 TOKEN_EOF = 7
 EOF = "@EOF@"
@@ -24,12 +30,12 @@ symbols = [';', ':', ',', '[', ']', '(', ')', '{', '}', '+', '-', '*', '=', '<',
 whitespace = ['\n', '\r', '\t', ' ', '\v', '\f']
 digit = [str(i) for i in range(10)]
 alphabet = [chr(i) for i in range(65, 91)] + [chr(i) for i in range(97, 123)]
-keywords = ['if', 'else', 'void', 'int', 'while', 'break', 'continue', 'switch', 'default', 'case', 'return']
+keywords = ['if', 'else', 'void', 'int', 'while', 'break', 'continue', 'switch', 'default', 'case', 'return', 'int']
 
 
 def get_string():
     global start_ind, end_ind, code
-    return code[start_ind:end_ind]  #TODO- end_int + 1 ????
+    return code[start_ind:end_ind]  # TODO- end_int + 1 ????
 
 
 def get_char():
@@ -44,8 +50,9 @@ def get_char():
     current_char = None
     return EOF
 
+
 def num():
-    print('num')
+    # print('num')
     global end_ind
     while current_char is not None:
 
@@ -60,7 +67,7 @@ def num():
 
 
 def symbol():
-    print('symbol')
+    # print('symbol')
     global end_ind
     if current_char == '=':
         if next_char == '=':
@@ -69,20 +76,22 @@ def symbol():
 
     get_char()
     end_ind = ind
-    print('get_string in symbols', get_string(), start_ind , '  ' , end_ind)
-    return True, get_string() , 'SYMBOl'
+    # print('get_string in symbols', get_string(), start_ind, '  ', end_ind)
+    return True, get_string(), 'SYMBOl'
 
 
 def skip_whitespace():
-    print('ws')
+    global line_num, line_changed
     global end_ind
     while current_char in whitespace:
+        if current_char == '\n':
+            line_num += 1
+            line_changed = True
         get_char()
         end_ind = ind
 
 
 def id():
-    print('id')
     global end_ind, ind
     while current_char is not None:
 
@@ -90,7 +99,6 @@ def id():
 
             if current_char in symbols or current_char in whitespace:
                 if get_string() in keywords:
-
                     return True, get_string(), 'KEYWORD'
 
                 return True, get_string(), 'ID'
@@ -99,19 +107,18 @@ def id():
         get_char()
         end_ind = ind
 
-
     return True, get_string(), 'ID'
 
 
 def comment():
-    print('comment')
+    # print('comment')
     global end_ind
     if next_char == '/':
-        print('this is a comment')
+        # print('this is a comment')
         get_char()
         end_ind = ind
         while current_char is not '\n' and current_char is not None:
-            print(current_char, 'is not the end of comment')
+            # print(current_char, 'is not the end of comment')
             get_char()
             end_ind = ind
 
@@ -133,55 +140,67 @@ def comment():
     get_char()
     return False, get_string() + current_char, 'invald input'
 
-def print_token(token_name, token_type):
-    print('<' , token_type, ' ' ,token_name , '>')
 
 def get_next_token():
     global state, end_ind, start_ind
     get_char()
+    is_token = False
+    token_string = token_type = ''
     while current_char is not None:
-        print(current_char)
+        skip_whitespace()
+        start_ind = ind
         if current_char in digit:
             state = TOKEN_NUM
             is_token, token_string, token_type = num()
-            if is_token:
-
-                # TODO add to file
-                pass
-            else:
-
-                # TODO handle error
-                get_char()
-
 
         elif current_char in symbols:
             state = TOKEN_SYMBOL
             is_token, token_string, token_type = symbol()
-            # TODO add to file
-            # start_ind = ind
-            # state = TOKEN_INITIAL
-
-        elif current_char in whitespace:  # TODO after while
-            skip_whitespace()
-            # start_ind = ind
-            # state = TOKEN_INITIAL
 
         elif current_char in alphabet:
             is_token, token_string, token_type = id()
-            if is_token:
-                # TOOD
-                pass
-            else:
-                get_char()
-                # TODO
 
         elif current_char == '/':
+            state = TOKEN_COMMENT
             is_token, token_string, token_type = comment()
-            if not is_token:
-                get_char()
-                # TODO
-        print_token(token_string, token_type)
+
+        if is_token and state != TOKEN_COMMENT:
+            print_token(token_type, token_string)
+        elif not is_token:
+            get_char()
+            print_error(token_type, token_string)
+
         start_ind = ind
         state = TOKEN_INITIAL
 
+
+def print_token(token_type, token_string):
+    global output_file, line_changed
+    if line_changed and line_num != 1:
+        print()
+    if line_changed:
+        print(str(line_num) + '. (' + token_type + ', ' + token_string + ')', end='')
+        line_changed = False
+    else:
+        print(' (' + token_type + ', ' + token_string + ')', end='')
+
+    # output_file.write(str(line_num) + '. (' + token_type + ', ' + token_string + ')\n')
+
+
+def print_error(token_type, token_string):
+    global error_file, line_changed
+    if line_changed and line_num != 1:
+        print()
+    if line_changed:
+        print(str(line_num) + '. (' + token_string + ', ' + token_type + ')', end='')
+        # line_changed = False
+    else:
+        print(' (' + token_string + ', ' + token_type + ')', end='')
+
+    # error_file.write(str(line_num) + '. (' + token_string + ', ' + token_type + ')')
+
+
 get_next_token()
+output_file.close()
+error_file.close()
+input_file.close()
